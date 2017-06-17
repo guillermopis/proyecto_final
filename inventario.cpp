b62a756b31e466596f4048117ac5ab1a43d0a56e
guillermopis <guillermo.pisqui@gmail.com>
2017-06-17 13:44:41 -0600

    actualizacion

--- inventario.cpp
+++ inventario.cpp
@@ -4,8 +4,22 @@
 #include <iostream>
 #include <time.h>
 #include <string.h>
+#include <windows.h>
+#include <iomanip>
 using namespace std;
+using std::left;
+using std::right;
 
+
+struct ventas{
+	int id;
+	int total;
+	int efectivo;
+	int cambio;
+	ventas *derecha;
+	ventas *izquierda;
+	ventas *padre;
+};
 //estructuras paa productos
 struct productos{
 	int id;
@@ -30,21 +44,30 @@ struct nodo{
 	nodo *padre;	
 };
 
-//prototipos de funcioes
+//prototipos de funcioes CLIENTES
 void menu();
 nodo *nuevonodo(int,int,char,char,nodo *); //funcion para crear nodos nuevos
 void insertarCliente(nodo *&,int,int,char[25],char[25],nodo *);
 void mostrarClientes(nodo *);
+bool buscarCliente(nodo *,int);
+void monstrarInformacion();
+
+//prototipos para ventas
+void nuevaVenta(int,int,int,int, ventas *);
+void insertarVenta(ventas *&,int,int,int,int,ventas *);
+void mostrarVenta(ventas *);
 
 //prototipos para productos
 void insertarProductos(productos *&,int,char[25],char[25],float,float,int,productos *);
 void mostrarProductos(productos *);
 nodo *nuevoproducto(int,char[25],char[25],float,float,int, productos *); //funcion para crear nodos nuevos
-
+bool buscarProducto( productos*,int);
 //definicion de los arboles BINARIOS
 nodo *arbol =NULL;
+nodo *clienteEncontrado= NULL;
 productos *arbolProductos = NULL;
-
+productos *productoEncontrado =NULL; //guardara el puntero al nodo que contenga el producto buscado.
+ventas * arbolVenta = NULL;
 //****************************************************************************************************************************
 //definicion de lista enlazada para almacenar proveedores
 class nodos {
@@ -121,6 +144,7 @@ void menu(){
 				//mostrar los clientes
 				system("cls");
 				cout<<" 		MOSTRANDO CLIENTES EXISTENTES"<<endl;
+				cout<<left<<setw(8)<<"ID."<<setw(15)<<"NIT"<<setw(15)<<"NOMBRE"<<setw(30)<<"DIRECCION"<<endl;
 				mostrarClientes(arbol);
 				system("pause");
 				break;
@@ -166,10 +190,70 @@ void menu(){
 				system("pause");
 				break;
 			}//cierra 	case 4
+			case 5:{
+				system("cls");
+				cout<<"				MOSTRADO VENTAS"<<endl<<endl;
+				cout<<left<<setw(8)<<"ID."<<setw(15)<<"TOTAL"<<setw(15)<<"EFECTIVO"<<setw(30)<<"CAMBIO"<<endl;
+				mostrarVenta(arbolVenta);
+				system("pause");
+				break;
+			}//fin del case 5
+			case 6: {
+				int id_ingresado=0,subTotal=0,total=0, cantidad=0, efectivo=0,cambio=0;
+				int id_produ=0;
+				char opcion[3];
+				system("cls");
+				cout<<"			REALIZANDO VENTA"<<endl;
+				cout<<"ingrese IDENTIFICADOR del cliente: "; cin>>id_ingresado;
+				//llamamos la funcion para buscar si existe el nit
+				if((buscarCliente(arbol,id_ingresado) )==true){
+						cout<<endl<<"NIT: "<<clienteEncontrado->nit<<endl;
+						cout<<"NOMBRE: "<<clienteEncontrado->nombre<<endl;
+						cout<<"DIRECCION: "<<clienteEncontrado->direccion<<endl;
+						//pedimos que ingrese los productos a comprar
+						do{
+							cout<<endl<<"ingrese el codigo del producto: "; cin>>id_produ;
+							if(buscarProducto(arbolProductos,id_produ) == true){
+								//si encuentra el producto dentro del arbol, mostrara la informacion de precio y existencia
+								cout<<"PRECIO_VENTA 				EXISTENCIA"<<endl;
+								cout<<"  "<<productoEncontrado->precio_venta<<"                                     "<<productoEncontrado->existencia<<endl;
+								//ahora le preguntamos cuantos quiere
+								cout<<endl<<"ingrese la cantidad a comprar: "; cin>>cantidad;
+								total=total + (productoEncontrado->precio_venta * cantidad);
+								cout<<"[ +++ PRODUCTO COMPRADO +++ ]"<<endl;
+							}else{
+								cout<<"producto no encontrado"<<endl;
+							}	
+							cout<<endl<<"desea agregar otro producto: s / n: "; cin>>opcion[1];
+						}while(opcion[1] != 'n');
+						//cuando salga del while le mostramos el resumen de la factura y guardamos la venta
+						system("cls");
+						cout<<"******************************* FACTURA ***********************************"<<endl;
+						cout<<endl<<"NIT: "<<clienteEncontrado->nit<<endl;
+						cout<<"NOMBRE: "<<clienteEncontrado->nombre<<endl;
+						cout<<"DIRECCION: "<<clienteEncontrado->direccion<<endl;
+						cout<<"****************************************************************************"<<endl;
+						cout<<endl<<"TOTAL A PAGAR: "<<total<<endl;
+						cout<<"ingrese efectivo: "; cin>>efectivo;
+						if(efectivo >=total){
+							cambio=efectivo-total;
+						}else{
+							cout<<"efectivo insuficiente"<<endl;
+						}
+						cout<<"CAMBIO: "<<cambio<<endl;
+						cout<<endl<<"------>     GRACIAS POR SU COMPRA   <------------"<<endl;
+						//guardamos la venta
+						insertarVenta(arbolVenta,id_ingresado,total,efectivo,cambio,NULL);
+						
+				}//fin del if de busqueda de cliente
+				system("pause");
+				break;
+			}//cierra el case 6
 			
 			case 7: {
 				system("cls")	;
 				cout<<"		MOSTRANDO PRODUCTOS EXISTENTES"<<endl<<endl;
+				cout<<left<<setw(8)<<"ID."<<setw(15)<<"CODIGO"<<setw(15)<<"DESCRIPCION"<<setw(15)<<"PRECIO_v"<<setw(15)<<"EXISTENCIA"<<endl;
 				mostrarProductos(arbolProductos);
 				system("pause");
 				break;
@@ -199,6 +283,93 @@ void menu(){
 		
 	}while(opcion!=9);
 } //cierra la funcion menu
+
+
+//nueva venta
+
+ventas *nuevaventa(int id, int total,int efectivo, int cambio, ventas *padre){
+    ventas *nueva_venta = new ventas();
+	
+	nueva_venta->id = id;
+	nueva_venta->total = total;
+	nueva_venta->efectivo=efectivo;
+	nueva_venta->cambio=cambio;
+	nueva_venta->derecha = NULL;
+	nueva_venta->izquierda = NULL;
+	nueva_venta->padre = padre;
+	
+	return nueva_venta;
+}
+
+//Funcion para insertar elementos en el arbol ventas
+void insertarVenta(ventas *&arbolVenta,int id, int total, int efectivo, int cambio, ventas *padre){
+	if(arbolVenta == NULL){ //Si el arbol esta vacio
+		ventas *nueva_venta = nuevaventa(id,total,efectivo,cambio,padre);
+		arbolVenta = nueva_venta;
+	}
+	else{ //Si el arbol tiene un nodo o mas
+		int valorRaiz = arbolVenta->id; //Obtenemos el valor de la raiz
+		if(id < valorRaiz){ //Si el elemento es menor a la raiz, insertamos en izq
+			insertarVenta(arbolVenta->izquierda,id,total,efectivo,cambio,arbolVenta);
+		}
+		else{//Si el elemento es mayor a la raiz, insertamos en der
+			insertarVenta(arbolVenta->derecha,id,total,efectivo,cambio,arbolVenta);
+		}	
+	}
+}
+//Funcion para mostrar el arbol ventas
+void mostrarVenta(ventas *arbolVenta){
+	if(arbolVenta == NULL){ //Si el arbol esta vacio
+		return;
+	}
+	else{
+		mostrarVenta(arbolVenta->derecha);
+		cout<<left<<setw(8)<<arbolVenta->id<<setw(15)<<arbolVenta->total<<setw(15)<<arbolVenta->efectivo<<setw(30)<<arbolVenta->cambio<<endl;			
+		//cout<<"identificador: "<<arbol->id<<endl;
+		//cout<<"NIT: "<<arbol->nit<<endl;
+		//cout<<"NOMBRE: "<<arbol->nombre<<endl;
+		//cout<<"DIRECCION: "<<arbol->direccion<<endl<<endl;
+		mostrarVenta(arbolVenta->izquierda);
+	}
+}
+
+
+//Funcion para buscar un elemento en el arbol productos
+bool buscarProducto(productos *arbolProductos,int n){
+	if(arbolProductos == NULL){ //si el arbol esta vacio
+		return false;
+	}
+	else if(arbolProductos->id == n){ //Si el nodo es igual al elemento
+		productoEncontrado=arbolProductos;	
+		return true;
+	}
+	else if(n < arbolProductos->id){
+		return buscarProducto(arbolProductos->izquierda,n);
+	}
+	else{
+		return buscarProducto(arbolProductos->derecha,n);
+	}
+}
+
+//Funcion para buscar un elemento en el arbol clientes
+bool buscarCliente(nodo *arbol,int n){
+	if(arbol == NULL){ //si el arbol esta vacio
+		return false;
+	}
+	else if(arbol->id == n){ //Si el nodo es igual al elemento
+		clienteEncontrado=arbol;	
+		return true;
+	}
+	else if(n < arbol->id){
+	
+		return buscarCliente(arbol->izq,n);
+		
+	}
+	else{
+		return buscarCliente(arbol->der,n);
+	}
+}
+
 
 //funcion para crear nodo
 nodo *nuevonodo(int id, int nit, char nombre[25], char direccion[25], nodo *padre){
@@ -243,10 +414,11 @@ void mostrarClientes(nodo *arbol){
 	}
 	else{
 		mostrarClientes(arbol->der);
-		cout<<"identificador: "<<arbol->id<<endl;
-		cout<<"NIT: "<<arbol->nit<<endl;
-		cout<<"NOMBRE: "<<arbol->nombre<<endl;
-		cout<<"DIRECCION: "<<arbol->direccion<<endl<<endl;
+		cout<<left<<setw(8)<<arbol->id<<setw(15)<<arbol->nit<<setw(15)<<arbol->nombre<<setw(30)<<arbol->direccion<<endl;			
+		//cout<<"identificador: "<<arbol->id<<endl;
+		//cout<<"NIT: "<<arbol->nit<<endl;
+		//cout<<"NOMBRE: "<<arbol->nombre<<endl;
+		//cout<<"DIRECCION: "<<arbol->direccion<<endl<<endl;
 		mostrarClientes(arbol->izq);
 	}
 }
@@ -327,12 +499,14 @@ void mostrarProductos(productos *arbolProductos){
 	}
 	else{
 		mostrarProductos(arbolProductos->derecha);
-		cout<<endl<<"ID_PRODUCTO: "<<arbolProductos->id<<endl;
-		cout<<"CODIGO_PRODUCTO: "<<arbolProductos->codigo<<endl;
-		cout<<"DESCRIPCION: "<<arbolProductos->descripcion<<endl;
-		cout<<"PRECIO COMPRA: "<<arbolProductos->precio_compra<<endl;
-		cout<<"PRECIO VENTA: "<<arbolProductos->precio_venta<<endl;
-		cout<<"EXISTENCIA: "<<arbolProductos->existencia<<endl;
+		
+		cout<<left<<setw(8)<<arbolProductos->id<<setw(15)<<arbolProductos->codigo<<setw(15)<<arbolProductos->descripcion<<setw(15)<<arbolProductos->precio_compra<<setw(10)<<arbolProductos->existencia<<endl;
+		//cout<<endl<<"ID_PRODUCTO: "<<arbolProductos->id<<endl;
+		//cout<<"CODIGO_PRODUCTO: "<<arbolProductos->codigo<<endl;
+		//cout<<"DESCRIPCION: "<<arbolProductos->descripcion<<endl;
+		//cout<<"PRECIO COMPRA: "<<arbolProductos->precio_compra<<endl;
+		//cout<<"PRECIO VENTA: "<<arbolProductos->precio_venta<<endl;
+		//cout<<"EXISTENCIA: "<<arbolProductos->existencia<<endl;
 		mostrarProductos(arbolProductos->izquierda);
 	}
 }
